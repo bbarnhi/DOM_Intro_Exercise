d07ae81f28fe4916178e6560e21adb51cc5cd4c3
bbarnhi <barnhill0126@gmail.com>
2020-06-27 09:47:16 -0400

    commit_6_27

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Javascript is alive!");
+// change greeting
+document.getElementById('greeting').innerText = "Hello World!"
+
+//Change background of li
+document.getElementById('essentials').children[0].style.backgroundColor = "yellow"
+
+//Add GIF to title
+let newVarName = document.createElement('img')
+newVarName.setAttribute('src', 'http:')
+document.getElementByID('greeting').appendChild(newVarName)
+
+//add new UL
+let newUl document.createElement('ul')
+    newUl.setAttribute('class', 'todo-items');
+    document.getElementById(newUl)
+    document.querySelector('body').appendChild(newUl)
+
+// 
+let items = ['array of text', 'more text', 'lots more text']
+
+for(const newText of items)
+let newLi document.createElement('li')
+    newLi.innerText = newText
+    document.querySelector('.todo-items').appendChild(newLi)
